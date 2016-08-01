218578e6767898e482053d2bce215953c380f5be
Lakaysiah Haddock <lakkaysiahsteele.Ls@gmail.com>
2016-08-01 19:29:42 -0400

    intial commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+	$("#hideButton").click(function(){
+		$("img").toggle();
+	});
+});
+$(document).ready(function(){
+	$("#toggleButton").click(function(){
+		$("img").toggle();
+	});
+});
+$(document).ready(function(){
+	$("#backButton").click(function(){
+		$("img").toggle();	
+	});
+});
+$(document).ready(function(){
+	$("#frontButton").click(function(){
+		$("img").toggle();	
+	});
+});		
+$(document).ready(function(){
+	$("#showButton").click(function(){
+		$("img").toggle();
+	});
+});
